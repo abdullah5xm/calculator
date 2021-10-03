e2ffeceb3fd09ea3a9ec0b5f19cce689ab875cd6
Samrat <67840234+samrat9xm@users.noreply.github.com>
2021-10-03 06:41:22 +0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,32 @@
+    //addition
+    function total1() {
+      let m = Number(document.getElementById('a').value);
+      let n = Number(document.getElementById('b').value);
+      let sum = m + n;
+      document.getElementById('sum').innerHTML = sum;
+    }
+
+    //substraction
+    function total2() {
+      let m = Number(document.getElementById('a').value);
+      let n = Number(document.getElementById('b').value);
+      let sum = m - n;
+      document.getElementById('sum').innerHTML = sum;
+    }
+
+    //Multiplication
+    function total3() {
+      let m = Number(document.getElementById('a').value);
+      let n = Number(document.getElementById('b').value);
+      let sum = m * n;
+      document.getElementById('sum').innerHTML = sum;
+    }
+
+    //Division
+    function total4() {
+      let m = Number(document.getElementById('a').value);
+      let n = Number(document.getElementById('b').value);
+      let sum = m / n;
+      document.getElementById('sum').innerHTML = sum;
+    }
+  
